4a9551cee86291260a060ad01cad98b8fa9dcd45
yojiyama7 <yojiyama7@gmail.com>
2023-01-25 18:21:17 +0900

    p

---
+++ atcoder/abc/abc247/c_1_2_1_3_1_2_1.nim
@@ -0,0 +1,56 @@
+import sequtils, strutils, strformat, strscans, algorithm, math, sugar, hashes, tables
+import complex, random, deques, heapqueue, sets, macros
+{. warning[UnusedImport]: off, hint[XDeclaredButNotUsed]: off .}
+
+template newSeqWith*(len: int, init: untyped): untyped =
+  var result = newSeq[typeof(init, typeOfProc)](len)
+  for i in 0 ..< len:
+    result[i] = init
+  move(result) # refs bug #7295
+
+# since (1, 1):
+template countIt*(s, pred: untyped): int =
+  var result = 0
+  for it {.inject.} in s:
+    if pred: result += 1
+  result
+
+macro toTuple[T](a: openArray[T], n: static[int]): untyped =
+  ## かなり原始的に書いている
+  ## より短くはできるが見てわかりやすいように
+  let tmp = genSym()
+  let t = newNimNode(nnkPar)
+  for i in 0..<n:
+    t.add(
+      newNimNode(nnkBracketExpr).add(
+        tmp,
+        newLit(i)
+      )
+    )
+  result = newNimNode(nnkStmtListExpr).add(
+    newNimNode(nnkLetSection).add(
+      newNimNode(nnkIdentDefs).add(
+        tmp,
+        newNimNode(nnkEmpty),
+        a
+      )
+    ),
+    t
+  )
+  # echo result.treeRepr
+
+proc just[T, U](x: T, f: T -> U): U =
+  return x.f
+
+################################
+
+let N = stdin.readline.parseInt()
+
+proc f(x: int): seq[int] =
+  if x == 1:
+    return @[1]
+  let beforeX = f(x-1)
+  # 足し算で描きたい
+  return concat(beforeX, @[x], beforeX)
+
+echo f(N).join(" ")

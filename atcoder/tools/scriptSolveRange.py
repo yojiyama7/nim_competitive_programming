d63ad1ce89f3254b111722874b4dff284de8cb7e
yojiyama7 <yojiyama7@gmail.com>
2023-02-13 22:20:05 +0900

    arc init

--- atcoder/tools/scriptSolveRange.py
+++ atcoder/tools/scriptSolveRange.py
@@ -3,3 +3,6 @@ from urlToDir import solve
 for i in range(1, 290):
     url = f"https://atcoder.jp/contests/abc{i:0>3}"
     solve(url)
+for i in range(1, 156):
+    url = f"https://atcoder.jp/contests/arc{i:0>3}"
+    solve(url)

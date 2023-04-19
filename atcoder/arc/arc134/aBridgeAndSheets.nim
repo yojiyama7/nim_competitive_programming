a52f1787f810725a7d2cc762f4f1a559e3035963
yojiyama7 <yojiyama7@gmail.com>
2023-04-19 19:47:22 +0900

    200

--- atcoder/arc/arc134/aBridgeAndSheets.nim
+++ atcoder/arc/arc134/aBridgeAndSheets.nim
@@ -1,82 +1,75 @@
-import sequtils, strutils, strformat, strscans, algorithm, math, sugar, hashes, tables
-import complex, random, deques, heapqueue, sets, macros
+import std/[sequtils, strutils, strformat, strscans, algorithm, math, sugar, hashes, tables, complex, random, deques, heapqueue, sets, macros]
 {. warning[UnusedImport]: off, hint[XDeclaredButNotUsed]: off, hint[Name]: off .}
+
+# {.since: (1, 1).}
+template countIt*(s, pred: untyped): int =
+  var result = 0
+  for it {.inject.} in s:
+    if pred: result += 1
+  result
+# {.since: (1, 1).}
+func maxIndex*[T](s: openArray[T]): int =
+  for i in 1..high(s):
+    if s[i] > s[result]: result = i
+# {.since: (1, 5, 1).}
+func ceilDiv*[T: SomeInteger](x, y: T): T {.inline.} =
+  when sizeof(T) == 8:
+    type UT = uint64
+  elif sizeof(T) == 4:
+    type UT = uint32
+  elif sizeof(T) == 2:
+    type UT = uint16
+  elif sizeof(T) == 1:
+    type UT = uint8
+  else:
+    {.fatal: "Unsupported int type".}
+  assert x >= 0 and y > 0
+  when T is SomeUnsignedInt:
+    assert x + y - 1 >= x
+  ((x.UT + (y.UT - 1.UT)) div y.UT).T
+# {.since: (1, 5, 1).}
+func euclMod[T: SomeNumber](x, y: T): T =
+  result = x mod y
+  if result < 0:
+    result += abs(y)
 
 template newSeqWith*(len: int, init: untyped): untyped =
   var result = newSeq[typeof(init, typeOfProc)](len)
   for i in 0 ..< len:
     result[i] = init
   move(result) # refs bug #7295
-
-# since (1, 1):
-template countIt*(s, pred: untyped): int =
-  var result = 0
-  for it {.inject.} in s:
-    if pred: result += 1
-  result
-# since (1, 1):
-func maxIndex*[T](s: openArray[T]): int =
-  for i in 1..high(s):
-    if s[i] > s[result]: result = i
-
-macro toTuple[T](a: openArray[T], n: static[int]): untyped =
-  ## かなり原始的に書いている
-  ## より短くはできるが見てわかりやすいように
-  let tmp = genSym()
-  let t = newNimNode(nnkPar)
+macro toTuple(lArg: openArray, n: static[int]): untyped =
+  let l = genSym()
+  var t = newNimNode(nnkTupleConstr)
   for i in 0..<n:
-    t.add(
-      newNimNode(nnkBracketExpr).add(
-        tmp,
-        newLit(i)
-      )
-    )
-  result = newNimNode(nnkStmtListExpr).add(
-    newNimNode(nnkLetSection).add(
-      newNimNode(nnkIdentDefs).add(
-        tmp,
-        newNimNode(nnkEmpty),
-        a
-      )
-    ),
-    t
-  )
-
-# 書き換えて使う想定
-const Modulo = 998244353
-type ModInt = distinct int
-
-proc toModInt(x: int): ModInt =
-  ModInt( ((x mod Modulo) + Modulo) mod Modulo )
-
-proc `$`(x: ModInt): string =
-  $(x.int)
-
-proc `+`(a, b: ModInt): ModInt =
-  (a.int + b.int).toModInt
-proc `+`(a: ModInt, b: int): ModInt =
-  (a.int + b).toModInt
-proc `+`(a: int, b: ModInt): ModInt =
-  (a + b.int).toModInt
-proc `-`(a, b: ModInt): ModInt =
-  (a.int - b.int).toModInt
-proc `-`(a: ModInt, b: int): ModInt =
-  (a.int - b).toModInt
-proc `-`(a: int, b: ModInt): ModInt =
-  (a - b.int).toModInt
-proc `*`(a, b: ModInt): ModInt =
-  (a.int * b.int).toModInt
-proc `*`(a: ModInt, b: int): ModInt =
-  (a.int * b).toModInt
-proc `*`(a: int, b: ModInt): ModInt =
-  (a * b.int).toModInt
-
-proc `+=`(a: var ModInt, b: int | ModInt): ModInt =
-  a = a + b
-proc `-=`(a: var ModInt, b: int | ModInt): ModInt =
-  a = a - b
-proc `*=`(a: var ModInt, b: int | ModInt): ModInt =
-  a = a * b
+    t.add quote do:
+      `l`[`i`]
+  quote do:
+    (let `l` = `lArg`; `t`)
 
 ################################
 
+let
+  (N, L, W) = stdin.readLine.split.map(parseInt).toTuple(3)
+  A = stdin.readLine.split.map(parseInt)
+
+var
+  coveredX = 0
+  i = 0
+  result = 0
+while coveredX < L:
+  if i < N and A[i] <= coveredX:
+    coveredX = max(A[i] + W, coveredX)
+    i += 1
+    # echo "Y"
+  else:
+    let rightX =  if i < N:
+                    A[i]
+                  else:
+                    L
+    let cnt = (rightX - coveredX).ceilDiv(W)
+    coveredX += W*cnt
+    result += cnt
+    # echo "N"
+  # echo (coveredX, i, result)
+echo result

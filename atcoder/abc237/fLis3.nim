a772a8fedbd0e1228c184eea84132590b1e9f12e
yojiyama7 <yojiyama7@gmail.com>
2024-05-11 07:24:33 +0900

    some

--- atcoder/abc237/fLis3.nim
+++ atcoder/abc237/fLis3.nim
@@ -1,82 +1,28 @@
-import sequtils, strutils, strformat, strscans, algorithm, math, sugar, hashes, tables
-import complex, random, deques, heapqueue, sets, macros
+import std/[sequtils, strutils, strformat, strscans, algorithm, math, sugar, hashes, tables, complex, random, deques, heapqueue, sets, macros, bitops]
 {. warning[UnusedImport]: off, hint[XDeclaredButNotUsed]: off, hint[Name]: off .}
 
-template newSeqWith*(len: int, init: untyped): untyped =
-  var result = newSeq[typeof(init, typeOfProc)](len)
-  for i in 0 ..< len:
-    result[i] = init
-  move(result) # refs bug #7295
-
-# since (1, 1):
-template countIt*(s, pred: untyped): int =
-  var result = 0
-  for it {.inject.} in s:
-    if pred: result += 1
-  result
-# since (1, 1):
-func maxIndex*[T](s: openArray[T]): int =
-  for i in 1..high(s):
-    if s[i] > s[result]: result = i
-
-macro toTuple[T](a: openArray[T], n: static[int]): untyped =
-  ## かなり原始的に書いている
-  ## より短くはできるが見てわかりやすいように
-  let tmp = genSym()
-  let t = newNimNode(nnkPar)
+macro toTuple(lArg: openArray, n: static[int]): untyped =
+  let l = genSym()
+  var t = newNimNode(nnkTupleConstr)
   for i in 0..<n:
-    t.add(
-      newNimNode(nnkBracketExpr).add(
-        tmp,
-        newLit(i)
-      )
-    )
-  result = newNimNode(nnkStmtListExpr).add(
-    newNimNode(nnkLetSection).add(
-      newNimNode(nnkIdentDefs).add(
-        tmp,
-        newNimNode(nnkEmpty),
-        a
-      )
-    ),
-    t
-  )
-
-# 書き換えて使う想定
-const Modulo = 998244353
-type ModInt = distinct int
-
-proc toModInt(x: int): ModInt =
-  ModInt( ((x mod Modulo) + Modulo) mod Modulo )
-
-proc `$`(x: ModInt): string =
-  $(x.int)
-
-proc `+`(a, b: ModInt): ModInt =
-  (a.int + b.int).toModInt
-proc `+`(a: ModInt, b: int): ModInt =
-  (a.int + b).toModInt
-proc `+`(a: int, b: ModInt): ModInt =
-  (a + b.int).toModInt
-proc `-`(a, b: ModInt): ModInt =
-  (a.int - b.int).toModInt
-proc `-`(a: ModInt, b: int): ModInt =
-  (a.int - b).toModInt
-proc `-`(a: int, b: ModInt): ModInt =
-  (a - b.int).toModInt
-proc `*`(a, b: ModInt): ModInt =
-  (a.int * b.int).toModInt
-proc `*`(a: ModInt, b: int): ModInt =
-  (a.int * b).toModInt
-proc `*`(a: int, b: ModInt): ModInt =
-  (a * b.int).toModInt
-
-proc `+=`(a: var ModInt, b: int | ModInt): ModInt =
-  a = a + b
-proc `-=`(a: var ModInt, b: int | ModInt): ModInt =
-  a = a - b
-proc `*=`(a: var ModInt, b: int | ModInt): ModInt =
-  a = a * b
+    t.add quote do:
+      `l`[`i`]
+  quote do:
+    (let `l` = `lArg`; `t`)
+proc pow(x, n, m: int): int =
+  if n == 0:
+    return 1
+  if n mod 2 == 1:
+    result = x * pow(x, n-1, m)
+  else:
+    result = pow(x, n div 2, m)^2
+  result = result mod m
+proc parseInt(c: char): int =
+  c.int - '0'.int
+iterator skipBy(r: HSlice, step: int): int =
+  for i in countup(r.a, r.b, step):
+    yield i
+proc initHashSet[T](): Hashset[T] = initHashSet[T](0)
 
 ################################
 

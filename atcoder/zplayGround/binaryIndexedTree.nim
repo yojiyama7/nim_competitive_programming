534e1dc6efaad02aa6f9e1989eca5578fbb83fec
yojiyama7 <yojiyama7@gmail.com>
2024-06-19 10:22:44 +0900

    s

--- atcoder/zplayGround/binaryIndexedTree.nim
+++ atcoder/zplayGround/binaryIndexedTree.nim
@@ -2,14 +2,27 @@
 # .push(x)      : 要素を最後尾に追加
 # .prefixSum(i) : i番目(0 or 1 idx)までの総和
 # .add(i, a)    : i番目にaを加算
+import bitops, sequtils
 
 type
   FenwickTree[T: int or float] = object
-    n: int
     l: seq[T]
 
-proc initFenwickTree[T: int or float](n: int): FenwickTree[T] =
-  result.n = n
+proc initFenwickTree[T: int or float](n: int): FenwickTree =
   result.l = newSeq[T](n)
+# proc `[]`[T: int or float](self: var FenwickTree[T], eidx) =
 
-echo initFenwickTree[int](10)
+proc sumFromOneTo[T: int or float](self: FenwickTree[T], bidx: int): int =
+  var x = bidx
+  while x > 0:
+    result += self.l[x]
+    x -= (x and -x)
+proc sumOf[T: int or float](self: FenwickTree[T], r: HSlice): int =
+  let (l, r) = (r.a, r.b+1)
+  sumFromOneTo(r) - sumFromOneTo(l)
+
+proc add[T: int or float](self: var FenwickTree[T], eidx: int, v: int) =
+  var x = eidx+1
+  while x <= self.l.len:
+    self.l[x] += v
+    x += (x and -x)

07bc9e452ca753caa084f58beda937373815b174
yojiyama7 <yojiyama7@gmail.com>
2024-09-13 14:46:18 +0900

    will green

---
+++ atcoder/abc370/bBinaryAlchemy.nim
@@ -0,0 +1,40 @@
+import std/[sequtils, strutils, strformat, strscans, algorithm, math, sugar, hashes, tables, complex, random, deques, heapqueue, sets, macros, bitops]
+{. warning[UnusedImport]: off, hint[XDeclaredButNotUsed]: off, hint[Name]: off .}
+
+macro toTuple(lArg: openArray, n: static[int]): untyped =
+  let l = genSym()
+  var t = newNimNode(nnkTupleConstr)
+  for i in 0..<n:
+    t.add quote do:
+      `l`[`i`]
+  quote do:
+    (let `l` = `lArg`; `t`)
+proc pow(x, n, m: int): int =
+  if n == 0:
+    return 1
+  if n mod 2 == 1:
+    result = x * pow(x, n-1, m)
+  else:
+    result = pow(x, n div 2, m)^2
+  result = result mod m
+proc parseInt(c: char): int =
+  c.int - '0'.int
+iterator skipBy(r: HSlice, step: int): int =
+  for i in countup(r.a, r.b, step):
+    yield i
+proc initHashSet[T](): Hashset[T] = initHashSet[T](0)
+
+################################
+
+let
+  N = stdin.readLine.parseInt()
+  A1 = newSeqWith(N, stdin.readLine.split.map(parseInt))
+
+var a = 0
+for i in 0..<N:
+  var b = i
+  if a < b:
+    swap(a, b)
+  a = A1[a][b]-1
+
+echo a+1

a8bf65d7c118b4c36140c0220eaca89fedb904be
yojiyama7@42tokyo <yojyamam@c4r2s14.42tokyo.jp>
2025-04-08 23:28:25 +0900

    abc385C

---
+++ atcoder/abc385/cIlluminateBuildings.nim
@@ -0,0 +1,50 @@
+import std/[sequtils, strutils, strformat, strscans, algorithm, math, sugar, hashes, tables, complex, random, deques, heapqueue, sets, macros, bitops]
+{. warning[UnusedImport]: off, hint[XDeclaredButNotUsed]: off, hint[Name]: off .}
+
+macro toTuple(lArg: openArray, n: static[int]): untyped =
+  let l = genSym()
+  var t = newNimNode(nnkTupleConstr)
+  for i in 0..<n:
+    t.add quote do:
+      `l`[`i`]
+  quote do:
+    (let `l` = `lArg`; `t`)
+proc pow(x, n, m: int): int =
+  if n == 0:
+    return 1
+  if n mod 2 == 1:
+    result = x * pow(x, n-1, m)
+  else:
+    result = pow(x, n div 2, m)^2
+  result = result mod m
+proc parseInt(c: char): int =
+  c.int - '0'.int
+iterator skipBy(r: HSlice, step: int): int =
+  for i in countup(r.a, r.b, step):
+    yield i
+proc initHashSet[T](): Hashset[T] = initHashSet[T](0)
+
+################################
+
+let
+  N = stdin.readLine.parseInt()
+  H = stdin.readLine.split.map(parseInt)
+
+var result = 1
+for step in 1..<N:
+  for offset in 0..<step:
+    var i = offset
+    var bh = -1
+    var score = 0
+    while (i < N):
+      # echo (step, offset, i, score)
+      if bh == -1 or bh == H[i]:
+        score += 1
+      else:
+        result = max(score, result)
+        score = 1
+      bh = H[i]
+      i += step
+    result = max(score, result)
+
+echo result
